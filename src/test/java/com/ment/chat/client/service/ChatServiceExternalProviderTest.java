945f51fb0db4c151222e3f65e34fa6016831d194
mansengstedt <mans.engstedt@gmail.com>
2025-11-24 22:07:28 +0100

    Fixed unused imports.

--- src/test/java/com/ment/chat/client/service/ChatServiceExternalProviderTest.java
+++ src/test/java/com/ment/chat/client/service/ChatServiceExternalProviderTest.java
@@ -2,23 +2,15 @@ package com.ment.chat.client.service;
 
 import com.ment.chat.client.model.enums.LlmProvider;
 import com.ment.chat.client.model.enums.LlmStatus;
-import com.ment.chat.client.model.in.CreateCompletionRequest;
-import com.ment.chat.client.model.out.CreateCompletionResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-
-import java.time.OffsetDateTime;
-import java.util.UUID;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,12 +33,9 @@ public class ChatServiceExternalProviderTest extends BaseChatServiceTest {
     @Test
     void statusOfProviders() {
         chatService.getAllProviderStatus().getStatusList()
-                .forEach(status -> {
-                    switch (status.getProvider()) {
-                        case OLLAMA, DOCKER -> assertThat(status.getStatus()).isEqualTo(LlmStatus.AVAILABLE);
-                        case OPENAI, ANTHROPIC -> assertThat(status.getStatus()).isEqualTo(LlmStatus.AVAILABLE);
-                    }
-                });
+                .forEach(status ->
+                    assertThat(status.getStatus()).isEqualTo(LlmStatus.AVAILABLE)
+                );
     }
 
 }
